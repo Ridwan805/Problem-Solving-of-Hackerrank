2769fc99a67aa9ce2f705ffad63ab00d80bbf715
Ridwanur Rahman <154875891+Ridwan805@users.noreply.github.com>
2024-05-04 20:24:27 +0600

    Create 2D Array - DS.py

---
+++ Data Structures/2D Array - DS/2D Array - DS.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    max_sum = -math.inf
+    count_sum = 0
+    for i in range(4):
+        for j in range(4):
+            count_sum = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            if max_sum < count_sum:
+                max_sum = count_sum
+    return max_sum   
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    99
